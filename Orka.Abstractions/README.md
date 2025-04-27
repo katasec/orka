300a537b237f10969f3d24bc3cdb5704a8c6c7ab
Ameer Deen <writeameer@gmail.com>
2025-04-27 14:59:01 +0400

    Cleaning basic setup

---
+++ Orka.Abstractions/README.md
@@ -0,0 +1,3 @@
+# Katasec.Orka.Abstractions
+
+This package contains shared abstractions for Orka resource handlers.
--- Orka.Abstractions/publish.ps1
+++ Orka.Abstractions/publish.ps1
@@ -1,24 +1,36 @@
 # Set variables
-$projectPath = "C:\path\to\Orka.Abstractions\Orka.Abstractions.csproj"
-$outputPath = "C:\path\to\output"
+$projectName = $PWD.Path.Split("\")[-1]
+$projectPath =  Join-Path $PWD.Path $projectName
+$outputPath = Join-Path $PWD.Path "bin" "release"
 $apiKey = "$env:NUGET_API_KEY"
 $nugetSource = "https://api.nuget.org/v3/index.json"
 
 # Make sure output directory exists
+$outputPath = Join-Path $PWD.Path "bin" "release"
 if (-not (Test-Path -Path $outputPath)) {
     New-Item -ItemType Directory -Path $outputPath
+} else {
+    Write-Output "Output directory already exists: $outputPath"
 }
+
+$gitTag = git describe --tags --abbrev=0
+if (-not $gitTag) {
+    Write-Output "❌ No Git tag found!"
+    exit 1
+}
+Write-Output "✅ Using Git tag as version: $gitTag"
+
 
 # Pack the project
 Write-Output "Project Path: $projectPath"
-dotnet pack $projectPath --output $outputPath --configuration Release
+dotnet pack .  --configuration Release /p:PackageVersion=$gitTag
 
 # Find the generated .nupkg
 $nupkg = Get-ChildItem -r -i "*.nupkg" | Select-Object -First 1
 
 if ($nupkg) {
     # Push the package
-    ##dotnet nuget push $nupkg.FullName --api-key $apiKey --source $nugetSource
+    dotnet nuget push $nupkg.FullName --api-key $apiKey --source $nugetSource
     Write-Output "✅ Package pushed: $($nupkg.FullName)"
 } else {
     Write-Output "❌ No package found to push."
